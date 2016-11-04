6025d7169fb2a56af33cfdfb669ea2b02f33ac7e
filin-from-2042 <filin2042@gmail.com>
2016-11-04 15:35:12 +0300

    заверстана карусель с товарами

--- js/script.js
+++ js/script.js
@@ -1,19 +1,15 @@
 function inanime_new() {
-    // инициализирует вертикальную карусель и сохраняет настройки в объекте одноименном с id html-обертки
     this.init_custom_vertical_carousel = function (element_id, elements_count) {
-        /* конфигурация */
         var carouselData =
         {
-            cc_count: elements_count, // количество изображений
-            cc_position: 0, // текущий сдвиг влево
+            cc_count: elements_count, // пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ
+            cc_position: 0, // пїЅпїЅпїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ пїЅпїЅпїЅпїЅпїЅ
             carouselEl: document.getElementById(element_id),
             cc_height: document.getElementById(element_id).querySelector('img').height,
             paddingBottom: parseInt($(document.getElementById(element_id).querySelector('li')).css('paddingBottom'))
         }
         inanime_new[element_id] = carouselData;
         carouselData.carouselEl.querySelector('.prev').onclick = function () {
-            // сдвиг влево
-            // последнее передвижение влево может быть не на 3, а на 2 или 1 элемент
             var prevPosition = inanime_new[element_id].cc_position;
             var height = inanime_new[element_id].cc_height;
             var paddingBottom = inanime_new[element_id].paddingBottom;
@@ -29,8 +25,6 @@ function inanime_new() {
 
         carouselData.carouselEl.querySelector('.next').onclick = function () {
             var listElems = inanime_new[element_id].carouselEl.querySelectorAll('li');
-            // сдвиг вправо
-            // последнее передвижение вправо может быть не на 3, а на 2 или 1 элемент
             var prevPosition = inanime_new[element_id].cc_position;
             var height = inanime_new[element_id].cc_height;
             var paddingBottom = inanime_new[element_id].paddingBottom;
@@ -43,11 +37,54 @@ function inanime_new() {
                 }
             );
         };
+    };
+    this.init_custom_horizontal_carousel = function (element_id, elements_count) {
+        var carouselData =
+        {
+            cc_count: elements_count,
+            cc_position: 0,
+            carouselEl: document.getElementById(element_id),
+            cc_width: $(document.getElementById(element_id).querySelector('li')).width(),
+            paddingRight: parseInt($(document.getElementById(element_id).querySelector('li')).css('paddingRight'))
+        };
 
+        inanime_new[element_id] = carouselData;
+        $('#'+element_id).css('width',(carouselData.cc_width+carouselData.paddingRight)*elements_count );
+        carouselData.carouselEl.querySelector('.prev').onclick = function () {
+            var prevPosition = inanime_new[element_id].cc_position;
+            var width = inanime_new[element_id].cc_width;
+            var paddingRight = inanime_new[element_id].paddingRight;
+            var count = inanime_new[element_id].cc_count;
+
+            inanime_new[element_id].cc_position = Math.min(prevPosition + (width + paddingRight) * count, 0);
+            $('#' + element_id + ' ul').animate(
+                {
+                    marginLeft: inanime_new[element_id].cc_position + 'px'
+                }
+            );
+        };
+
+        carouselData.carouselEl.querySelector('.next').onclick = function () {
+            var listElems = inanime_new[element_id].carouselEl.querySelectorAll('li');
+            var prevPosition = inanime_new[element_id].cc_position;
+            var width = inanime_new[element_id].cc_width;
+            var paddingRight = inanime_new[element_id].paddingRight;
+            var count = inanime_new[element_id].cc_count;
+
+            inanime_new[element_id].cc_position = Math.max(prevPosition - (width + paddingRight) * count, -(width + paddingRight) * (listElems.length - count));
+            $('#' + element_id + ' ul').animate(
+                {
+                    marginLeft: inanime_new[element_id].cc_position + 'px'
+                }
+            );
+        };
     }
 }
 $(document).ready(function () {
     window.inanime_new = new inanime_new();
 
     inanime_new.init_custom_vertical_carousel('carousel-custom-vertical', 2);
+
+    if(window.innerWidth >= 760 )inanime_new.init_custom_horizontal_carousel('carousel-custom-horizontal-books', 3);
+    else  inanime_new.init_custom_horizontal_carousel('carousel-custom-horizontal-books', 2);
 });
