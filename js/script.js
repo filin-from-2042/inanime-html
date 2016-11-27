3bc5296597430bf458a198ea7539e7dceaef06b3
filin-from-2042 <filin2042@gmail.com>
2016-11-27 11:43:59 +0300

    Оформление заказа доделано

--- js/script.js
+++ js/script.js
@@ -122,12 +122,12 @@ function inanime_new() {
             );
         });
 
-    }
+    };
 
     this.ddSetSelectedText = function (element)
     {
         $(element).closest(".dropdown").find(".btn.dropdown-toggle").text('').append('<span class="glyphicon glyphicon-chevron-down"></span><span class="text">'+element.innerHTML+'</span>')/*.text(element.innerHTML)*/;
-    }
+    };
 
     this.radioClick = function (event)
     {
@@ -136,7 +136,19 @@ function inanime_new() {
         radioButton.closest('.radio-container').find('input.ia-radio-value').val(radioButton.find('span.value.hidden').text());        
         radioButton.closest('.radio-container').find('.ia-radio-button').removeClass('active');
         radioButton.addClass('active');
-    }
+    };
+
+    this.radioValuesClick = function()
+    {
+        if ($(this).hasClass('ia-radio-button')) var radioButton = $(this);
+        else var radioButton = $(this).closest('.radio-button-container').find('.ia-radio-button');
+        var radioValue = radioButton.find('span.value.hidden').text();
+        var valuesContainer = radioButton.closest('.radio-values-container');
+        valuesContainer.find('.values-container > div').removeClass('selected');
+        valuesContainer.find('.values-container input').attr('disabled',true);
+        valuesContainer.find('.values-container .'+radioValue).addClass('selected');
+        valuesContainer.find('.values-container .'+radioValue+' input').attr('disabled',false);
+    };
 
     this.counterButtonClick = function ()
     {
@@ -151,7 +163,7 @@ function inanime_new() {
         {
             if (input.val() > 1) input.val(parseInt(input.val()) - 1);
         }
-    }
+    };
 
     this.questionClick = function ()
     {
@@ -165,6 +177,7 @@ window.inanime_new = new inanime_new();
 $(document).ready(function ()
 {
     $('.ia-radio-button,.radio-button-container .button-title').click(inanime_new.radioClick);
+    $('.radio-values-container .ia-radio-button,.radio-values-container .radio-button-container .button-title').click(inanime_new.radioValuesClick);
     $('.ia-counter-container .button').click(inanime_new.counterButtonClick);
     $('#questions .question-answer-section-container .question-title').click(inanime_new.questionClick);
 });
